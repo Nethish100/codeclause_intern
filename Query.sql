a067d4bcbbb1e28e82cc9e6d53f633d5f56be7ae
Nethish100 <109594526+Nethish100@users.noreply.github.com>
2023-09-26 13:14:02 +0530

    Add files via upload

---
+++ Query.sql
@@ -0,0 +1,14 @@
+create database library;
+drop database library;
+
+select * from books;
+insert into books value
+(11,"c++","jain",2),(112,"DS","shree",1),
+(12,"java","tg",1),(114,"m4","Dubner",1),
+(13,"CN","shree",1),(116,"ML","deep",1),
+(14,"cloud","s boss",1);
+
+select * from students;
+insert into students value
+(6110,"nitti",1),(6111,"hitti",2),(6120,"ujju",4),
+(6123,"jain",0);
